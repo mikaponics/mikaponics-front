0801888a8a2dba6a9d6262f137fcb2eb5d3076bc
bartmika <bart@mikasoftware.com>
2019-03-23 11:08:46 -0400

    Latest progress with the device details page.

--- src/components/deviceComponent.js
+++ src/components/deviceComponent.js
@@ -1,11 +1,97 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
+class InstrumentTable extends Component {
+    render() {
+        function get(dict, key) {
+            return dict.hasOwnProperty(key) ? dict[key] : null;
+        }
+
+        const { instrument } = this.props;
+
+        if (instrument === undefined) {
+            return null;
+        }
+
+        var lastMeasuredValue = get(instrument, "lastMeasuredValue");
+        var lastMeasuredAtUtc = get(instrument, "lastMeasuredAtUtc");
+        var unitOfMeasure = get(instrument, "unitOfMeasure");
+
+        return (
+            <div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Last measured value</th>
+                                    <td>
+                                        {lastMeasuredValue} {unitOfMeasure}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Last measured time</th>
+                                    <td>
+                                        {lastMeasuredAtUtc}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class DeviceSummaryTable extends Component {
+    render() {
+        const { device } = this.props;
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Status</th>
+                                <td>{device.state}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Last measured value</th>
+                                <td>{device.lastMeasuredValue} {device.lastMeasuredUnitOfMeasure}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Last measured time</th>
+                                <td>{device.lastMeasuredTimestamp}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Last measured by</th>
+                                <td>{device.lastMeasuredInstrumentTypeOf}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
 
 class DeviceComponent extends Component {
     render() {
         const { device } = this.props;
-        console.log("--------->", device);
         return (
             <div>
                 <nav aria-label="breadcrumb">
@@ -20,62 +106,17 @@ class DeviceComponent extends Component {
                 <hr />
 
                 <h2>Device Summary</h2>
-                <div className="row">
-                    <div className="col-md-12">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col"></th>
-                                    <th scope="col"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">Status</th>
-                                    <td>{device.state}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Last measured value</th>
-                                    <td>{device.lastMeasuredValue} {device.lastMeasuredUnitOfMeasure}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Last measured time</th>
-                                    <td>{device.lastMeasuredTimestamp}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Last measured by</th>
-                                    <td>{device.lastMeasuredInstrumentTypeOf}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <DeviceSummaryTable device={device} />
+
+                <br />
 
                 <h2>Humidity</h2>
-                <div className="row">
-                    <div className="col-md-12">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col"></th>
-                                    <th scope="col"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">Last measured value</th>
-                                    <td>
-                                        {device.humidity.lastMeasuredValue} {device.humidity.unitOfMeasure}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">Last measured time</th>
-                                    <td>{device.humidity.lastMeasuredTimestamp}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <InstrumentTable instrument={device.humidity} />
+
+                <br />
+
+                <h2>Temperature</h2>
+                <InstrumentTable instrument={device.temperature} />
 
             </div>
         );
