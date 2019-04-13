36671096cd6a1ed99d2cf75137d84b046844264d
bartmika <bart@mikasoftware.com>
2019-04-13 19:10:38 -0400

    Updated component list for alerts.

--- src/components/instruments/instrumentAnalysisListComponent.js
+++ src/components/instruments/instrumentAnalysisListComponent.js
@@ -15,29 +15,31 @@ class InstrumentReportTable extends Component {
             console.log(row)
             elements.push(
                 <tr key={row.slug}>
-                    <th scope="row">{row.createdAt}</th>
-                    <th scope="row">{row.startDt}</th>
-                    <th scope="row">{row.finishDt}</th>
-                    <th scope="row">
-                        <Link to={row.absoluteUrl}>View</Link>
+                    <th>{row.createdAt}</th>
+                    <th>{row.startDt}</th>
+                    <th>{row.finishDt}</th>
+                    <th>
+                        <Link to={row.absoluteUrl}>View&nbsp;<i className="fas fa-chevron-right"></i></Link>
                     </th>
                 </tr>
             );
         }
         return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Created</th>
-                        <th scope="col">From</th>
-                        <th scope="col">To</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {elements}
-                </tbody>
-            </table>
+            <div className="table-responsive">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Created</th>
+                            <th scope="col">From</th>
+                            <th scope="col">To</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {elements}
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
@@ -51,27 +53,44 @@ class InstrumentAnalysisListComponent extends Component {
                 <nav aria-label="breadcrumb">
                     <ol className="breadcrumb">
                         <li className="breadcrumb-item">
-                           <Link to="/dashboard">Dashboard</Link>
+                           <Link to="/dashboard"><i className="fas fa-tachometer-alt"></i>&nbsp;Dashboard</Link>
+                        </li>
+                        <li className="breadcrumb-item">
+                           <Link to="/devices"><i className="fas fa-cubes"></i>&nbsp;Devices</Link>
                         </li>
                         <li className="breadcrumb-item">
                             {instrument.absoluteParentUrl &&
-                                <Link to={`${instrument.absoluteParentUrl}`}>Device</Link>
+                                <Link to={`${instrument.absoluteParentUrl}`}>
+                                    <i className="fas fa-cube"></i>&nbsp;Device
+                                </Link>
                             }
                         </li>
                         {instrument.absoluteUrl &&
                             <li className="breadcrumb-item">
-                                <Link to={`${instrument.absoluteUrl}`}>Instrument</Link>
+                                <Link to={`${instrument.absoluteUrl}`}>
+                                    <i className={`fas fa-${instrument.icon}`}></i>&nbsp;Instrument
+                                </Link>
                             </li>
                         }
-                        <li className="breadcrumb-item active" aria-current="page">Analyses</li>
+                        <li className="breadcrumb-item active" aria-current="page">
+                            <i className="fas fa-flask"></i>&nbsp;Analyses
+                        </li>
                     </ol>
                 </nav>
-                <h1>Analyses</h1>
-                <hr />
+                <h1><i className="fas fa-flask"></i>&nbsp;Analyses</h1>
+
+                <div className="buttons-card">
+                    <section className="row text-center placeholders">
+                        <div className="rounded-circle circle-200 bg-pink text-center">
+                            <Link to={`${instrument.absoluteUrl}/create-analysis`} className="d-block link-ndecor" title="Generate Analysis">
+                                <span className="r-circle"><i className="fas fa-lightbulb fa-3x"></i></span>
+                            </Link>
+                        </div>
+                    </section>
+                </div>
+
                 <div className="row">
                     <div className="col-md-12">
-                        <Link to={`${instrument.absoluteUrl}/create-analysis`}>Create Analysis</Link>
-
                         <InstrumentReportTable instrumentAnalysisList={instrumentAnalysisList} />
                     </div>
                 </div>
