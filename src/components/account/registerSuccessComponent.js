3cf1d2555c0ece8dac14406109af423f67874327
bartmika <bart@mikasoftware.com>
2019-04-16 15:29:35 -0400

    Fixed type.

--- src/components/account/registerSuccessComponent.js
+++ src/components/account/registerSuccessComponent.js
@@ -8,7 +8,9 @@ class RegisterSuccessComponent extends React.Component {
             <div className="container">
                 <main id="main">
                     <div className="jumbotron">
-                        <h1 className="display-4">Registed</h1>
+                        <h1 className="display-4">
+                           <i className="fas fa-check"></i>&nbsp;Registered
+                        </h1>
                         <p className="lead">Thank you for registration, an activation email has been sent to your inbox. Please follow the instructions provided to activate your account.</p>
                         <hr className="my-4" />
                         <p>Once you have been activated, you will be able to log into the system.</p>
