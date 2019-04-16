451a17404d79597d5fc2c66a8eff73521722a437
bartmika <bart@mikasoftware.com>
2019-04-16 15:01:29 -0400

    Improved activation message.

--- src/components/account/activateComponent.js
+++ src/components/account/activateComponent.js
@@ -9,10 +9,12 @@ class ActivateComponent extends React.Component {
                 <main id="main">
                     <div className="jumbotron">
                         <h1 className="display-4">Activated</h1>
-                        <p className="lead">Your account has begin activated, you may now loggin.</p>
+                        <p className="lead">Your account has begin <strong>activated</strong>, you may now begin using your account. Click the button below to get started.</p>
 
                         <p className="lead">
-                            <Link className="btn btn-primary btn-lg" to="/onboard">Dashboard</Link>
+                            <Link className="btn btn-primary btn-lg" to="/onboard">
+                                <i className="fas fa-tachometer-alt"></i>&nbsp;Dashboard
+                            </Link>
                         </p>
                     </div>
                 </main>
