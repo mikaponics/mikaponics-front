0d53d24d2217665e93e9d963af7ec13fae274650
bartmika <bart@mikasoftware.com>
2019-04-27 12:11:19 -0400

    Switched the data page in the instruments order.

--- src/components/instruments/instrumentComponent.js
+++ src/components/instruments/instrumentComponent.js
@@ -178,6 +178,21 @@ class InstrumentComponent extends Component {
                     <div className="col-sm-4">
                         <div className="card box-shadow text-center mx-auto">
                             <div className="card-custom-top-2">
+                                <i className="fas fa-cloud fa-3x"></i>
+                            </div>
+                            <div className="card-body">
+                                <h3 className="card-title">Data</h3>
+                                <p className="card-text">List all the time series data and download it to CSVs.</p>
+                                <Link to={`/instrument/${instrument.slug}/data`}className="btn btn-success btn-lg">
+                                    View&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-4">
+                        <div className="card box-shadow text-center mx-auto">
+                            <div className="card-custom-top-2">
                                 <i className="fas fa-bell fa-3x"></i>
                             </div>
                             <div className="card-body">
@@ -199,21 +214,6 @@ class InstrumentComponent extends Component {
                                 <h3 className="card-title">Analyses</h3>
                                 <p className="card-text">Run analysis reports on this instrument.</p>
                                 <Link to={`/instrument/${instrument.slug}/analyses`}className="btn btn-success btn-lg">
-                                    View&nbsp;<i className="fas fa-arrow-circle-right"></i>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-sm-4">
-                        <div className="card box-shadow text-center mx-auto">
-                            <div className="card-custom-top-2">
-                                <i className="fas fa-cloud fa-3x"></i>
-                            </div>
-                            <div className="card-body">
-                                <h3 className="card-title">Data</h3>
-                                <p className="card-text">List all the time series data and download it to CSVs.</p>
-                                <Link to={`/instrument/${instrument.slug}/data`}className="btn btn-success btn-lg">
                                     View&nbsp;<i className="fas fa-arrow-circle-right"></i>
                                 </Link>
                             </div>
