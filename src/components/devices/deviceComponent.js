82f92bea31c8687d1c93673a8e5e127cefe1dfd1
bartmika <bart@mikasoftware.com>
2019-05-17 20:54:06 -0400

    Re-designed the device details page.

--- src/components/devices/deviceComponent.js
+++ src/components/devices/deviceComponent.js
@@ -6,90 +6,62 @@ import 'moment-timezone';
 import { FlashMessageComponent } from "../flashMessageComponent";
 
 
-class InstrumentTable extends Component {
+class DeviceInstrumentTableRowComponent extends Component {
     render() {
-        const { title, icon, instrument } = this.props;
-
-        // DEFENSIVE CODE: Do not run this component if any of these errors occur.
-        if (instrument === undefined || instrument === null) { return null; }
-        if (instrument.lastMeasuredPrettyValue === undefined || instrument.lastMeasuredPrettyValue === null) { return null; }
-        if (instrument.lastMeasuredPrettyAt === undefined || instrument.lastMeasuredPrettyAt === null) { return null; }
-
+        const { instrument } = this.props;
         return (
-            <div>
-                <div className="row">
-                    <div className="col-md-12">
-
-                        <table className="table table-bordered custom-cell-w">
-                            <tbody>
-                                <tr className="bg-dark">
-                                    <th scope="row" colSpan="2" className="text-light"><i className={icon}></i>&nbsp;{title}</th>
-                                </tr>
-                                <tr>
-                                    <th scope="row" className="bg-light">Last measured value</th>
-                                    <td>{instrument.lastMeasuredPrettyValue}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" className="bg-light">Last measured time</th>
-                                    <td>
-                                        <Moment tz={instrument.timezone} format="YYYY/MM/DD hh:mm:ss a">
-                                            {instrument.lastMeasuredPrettyAt}
-                                        </Moment>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        )
+            <tr>
+                <td>
+                    <i className={`fas fa-${instrument.icon}`}></i>
+                </td>
+                <td>
+                    {instrument.name}
+                </td>
+                <td>
+                    <Moment tz={instrument.timezone} format="YYYY/MM/DD hh:mm:ss a">
+                        {instrument.lastMeasuredPrettyAt}
+                    </Moment>
+                </td>
+                <td>
+                    <Link to={`/instrument/${instrument.slug}`} className="btn btn-primary btn-sm">
+                        View&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                    </Link>
+                </td>
+            </tr>
+        );
     }
 }
 
-class DeviceSummaryTable extends Component {
+
+class DeviceInstrumentsTableComponent extends Component {
     render() {
-        const { device } = this.props;
-
-        // DEFENSIVE CODE: Do not run this component if any of these errors occur.
-        if (device === undefined || device === null) { return null; }
-        if (device.state === undefined || device.state === null) { return null; }
-        if (device.lastMeasuredPrettyValue === undefined || device.lastMeasuredPrettyValue === null) { return null; }
-        if (device.timezone === undefined || device.timezone === null) { return null; }
-        if (device.lastMeasuredPrettyAt === undefined || device.lastMeasuredPrettyAt === null) { return null; }
-
+        const { instruments } = this.props;
         return (
             <div className="row">
                 <div className="col-md-12">
-
-                    <table className="table table-bordered custom-cell-w">
+                <h2><i className="fas fa-microchip"></i>&nbsp;Instruments</h2>
+                <div className="table-responsive">
+                    <table className="table table-striped">
+                        <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Last Modified</th>
+                            <th></th>
+                        </tr>
+                        </thead>
                         <tbody>
-                            <tr className="bg-dark">
-                                <th scope="row" colSpan="2" className="text-light"><i className="fas fa-cube"></i>&nbsp;Device Summary</th>
-                            </tr>
-                            <tr>
-                                <th scope="row" className="bg-light">Status</th>
-                                <td>{device.state}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row" className="bg-light">Last measured value</th>
-                                <td>{device.lastMeasuredPrettyValue}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row" className="bg-light">Last measured time</th>
-                                <td>
-                                    <Moment tz={device.timezone} format="YYYY/MM/DD hh:mm:ss a">
-                                        {device.lastMeasuredPrettyAt}
-                                    </Moment>
-                                </td>
-                            </tr>
+                        {instruments.map(
+                            (instrument, i) => <DeviceInstrumentTableRowComponent instrument={instrument} key={i} />)
+                        }
                         </tbody>
                     </table>
-
                 </div>
-            </div>
-        )
+            </div></div>
+        );
     }
 }
+
 
 class DeviceComponent extends Component {
     render() {
@@ -111,71 +83,18 @@ class DeviceComponent extends Component {
 
                 <FlashMessageComponent object={flashMessage} />
 
-                <div className="card-group row">
-                    <div className="col-sm-4">
-                        <div className="card box-shadow text-center mx-auto">
-                            <div className="card-custom-top-2">
-                                <i className="fas fa-info fa-3x"></i>
-                            </div>
-                            <div className="card-body">
-                                <h3 className="card-title">Profile</h3>
-                                <p className="card-text">Set the name and various operating details about this device.</p>
-                                {device.slug &&
-                                    <Link to={`/device/${device.slug}/profile`} className="btn btn-success btn-lg">
-                                        View&nbsp;<i className="fas fa-arrow-circle-right"></i>
-                                    </Link>
-                                }
-                            </div>
+                <section className="row text-center placeholders">
+                    <div className="col-sm-3 placeholder">
+                        <div className="rounded-circle mx-auto mt-4 mb-4 circle-200 bg-pink">
+                            <Link to={`/device/${device.slug}/profile`} className="d-block link-ndecor" title="Add Client">
+                                <span className="r-circle"><i className="fas fa-pencil-alt fa-3x"></i></span>
+                            </Link>
                         </div>
+                        <h4>Profile</h4>
+                        <div className="text-muted">View your device profile</div>
                     </div>
-                    <div className="col-sm-4">
-                        <div className="card box-shadow text-center mx-auto">
-                            <div className="card-custom-top-2">
-                                <i className="fas fa-tint fa-3x"></i>
-                            </div>
-                            <div className="card-body">
-                                <h3 className="card-title">Humidity</h3>
-                                <p className="card-text">Set the name and various operating details about this device.</p>
-                                {device.humidity &&
-                                    <Link to={`/instrument/${device.humidity.slug}`} className="btn btn-success btn-lg">
-                                        View&nbsp;<i className="fas fa-arrow-circle-right"></i>
-                                    </Link>
-                                }
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-4">
-                        <div className="card box-shadow text-center mx-auto">
-                            <div className="card-custom-top-2">
-                                <i className="fas fa-thermometer-half fa-3x"></i>
-                            </div>
-                            <div className="card-body">
-                                <h3 className="card-title">Temperature</h3>
-                                <p className="card-text">Set the name and various operating details about this device.</p>
-                                {device.temperature &&
-                                    <Link to={`/instrument/${device.temperature.slug}`} className="btn btn-success btn-lg">
-                                        View&nbsp;<i className="fas fa-arrow-circle-right"></i>
-                                    </Link>
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <br />
-                <h2><i className="fas fa-table"></i>&nbsp;Device Summary</h2>
-                <DeviceSummaryTable device={device} />
-                <InstrumentTable
-                    title="Humidty"
-                    icon="fas fa-tint"
-                    instrument={device.humidity}
-                />
-                <InstrumentTable
-                    title="Temperature"
-                    icon="fas fa-thermometer-half"
-                    instrument={device.temperature}
-                />
-
+                </section>
+                <DeviceInstrumentsTableComponent instruments={device.instruments} />
             </div>
         );
     }
