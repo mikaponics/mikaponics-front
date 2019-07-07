cb721277056b18de4208580f02c7c6a76a8bc8f5
bartmika <bart@mikasoftware.com>
2019-07-06 23:06:38 -0400

    bugfix

--- src/validations/productionTerminateValidator.js
+++ src/validations/productionTerminateValidator.js
@@ -52,10 +52,6 @@ export function validateStep2Input(data) {
             errors.harvestQuality = 'This field is required.';
         }
     }
-    if ( data.wasLeftAliveAfterHarvest === undefined || data.wasLeftAliveAfterHarvest === null || data.wasLeftAliveAfterHarvest === "" || data.wasLeftAliveAfterHarvest === "null") {
-        errors.wasLeftAliveAfterHarvest = 'This field is required.';
-    }
-
     return {
         errors,
         isValid: isEmpty(errors)
